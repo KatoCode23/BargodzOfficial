f00e6e58fe8ff09bd968a940b65999d23767d074
KatoCode23 <Kalen23Imani@gmail.com>
2017-08-24 01:10:05 -0400

    Bargodz skeleton complete

---
+++ app.js
@@ -0,0 +1,32 @@
+var app = angular.module('app', []);
+
+app.controller('myController', function($scope){
+
+
+$scope.bargodz = [
+   {
+      name: 'Also Fit',
+      img1: '.jpg'
+   },
+   {
+      name: 'D',
+      img1: '.jpg'
+   },
+   {
+      name: 'Ray',
+      img1: '.jpg'
+   },
+   {
+      name: 'Dio',
+      img1: '.jpg'
+   },
+   {
+      name: 'Sky',
+      img1: '.jpg'
+
+   },
+   {
+      name: 'Lil Mann',
+      img1: '.jpg'
+   }
+});
